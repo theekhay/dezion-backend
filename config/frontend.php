52b420a224ce137017c2b66dbc40916161299c5f
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2019-02-09 17:25:40 +0100

    update to verify email link

--- config/frontend.php
+++ config/frontend.php
@@ -5,7 +5,7 @@
         'url' => env('FRONTEND_URL', 'http://localhost:8080'),
 
         // path to my frontend page with query param queryURL(temporarySignedRoute URL)
-        'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/admin/verify/email?queryURL='),
+        'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/account/verify/email?queryURL='),
     ];
 
 ?>
