c5699ca9470a1e142adf0b7c0a392e893c662a49
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2019-02-09 23:13:22 +0100

    verificaton bug fix

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@login');
 
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 // Route::group(['middleware'=> ['auth:api'], 'prefix' => 'v1'], function()
 // {
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 
 //Auth::routes();
-//Auth::routes(['verify' => true]);
+Auth::routes(['verify' => true]);
 
 //Route::get('/home', 'HomeController@index');
 
